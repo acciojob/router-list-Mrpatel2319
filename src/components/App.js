bc76f7d79950fbf0a1a42609efa64245478d046f
Mritunjay Patel <97620757+Mrpatel2319@users.noreply.github.com>
2025-03-30 17:23:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
-
 import React from "react";
-import './../styles/App.css';
+import { BrowserRouter, Link, Route, Routes, useParams } from "react-router-dom";
 
-const App = () => {
+const Header = () => {
+  return (
+    <header>
+    <h1>Item List</h1>
+      <ul>
+        <li><Link to="/item/1">Item 1</Link></li>
+        <li><Link to="/item/2">Item 2</Link></li>
+        <li><Link to="/item/3">Item 3</Link></li>
+      </ul>
+    </header>
+  );
+};
+
+const Item = () => {
+  const { id } = useParams();
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Item {id}</h1>
+      <p>Description for Item {id}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const RouterList = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/item/:id" element={<Item />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RouterList;
